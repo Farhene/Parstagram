db6f29f6f427baf0cde6c579e8a412e21ea32f6b
Farhene Sultana <farhenesultana@gmail.com>
2021-03-01 13:38:44 -0500

    Set up Logging in to staying logged in. I also set up the logging out feature.

--- Parstagram/SceneDelegate.swift
+++ Parstagram/SceneDelegate.swift
@@ -6,17 +6,29 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if PFUser.current() != nil {//if logged in already switch to other view controller.
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            //self.window = UIWindow(windowScene: windowScene)
+            
+            self.window?.rootViewController = main.instantiateViewController(identifier: "FeedNavigationController")
+            self.window?.makeKeyAndVisible()
+        
+            
+            //self.window?.rootViewController = feedNavigationController
+            
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
